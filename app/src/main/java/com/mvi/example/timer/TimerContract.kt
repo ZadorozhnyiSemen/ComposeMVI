4f6dfea280043e822b6f692d6c6cae8cb8ee9fec
semyon <s.zadorozhnyi>
2022-08-10 16:33:27 +0400

    Fix style

--- app/src/main/java/com/mvi/example/timer/TimerContract.kt
+++ app/src/main/java/com/mvi/example/timer/TimerContract.kt
@@ -6,15 +6,15 @@ import com.mvi.mvi.contract.State
 
 internal data class TimerState(
     val timeLeft: Int = 0,
-	val completed: Boolean = false,
-): State
+    val completed: Boolean = false,
+) : State
 
 internal sealed interface TimerIntent : Intent {
-	data class StartTimer(val from: Int) : TimerIntent
-	data class UpdateTimer(val newTime: Int) : TimerIntent
-	object TimeIsUp : TimerIntent
+    data class StartTimer(val from: Int) : TimerIntent
+    data class UpdateTimer(val newTime: Int) : TimerIntent
+    object TimeIsUp : TimerIntent
 }
 
 internal sealed interface TimerSingleEvent : SingleEvent {
-	object ShowNotification : TimerSingleEvent
+    object ShowNotification : TimerSingleEvent
 }
